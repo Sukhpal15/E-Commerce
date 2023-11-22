bf69afeef2bf60a6afeab6e0b18f55119d298940
Sukhpal <sukhpal151515@gmail.com>
2023-11-22 18:57:16 +0530

    Form

--- Tasks/javascript/6_Nov/questions.js
+++ Tasks/javascript/6_Nov/questions.js
@@ -1,29 +1,18 @@
 const nextButton = document.getElementById("next");
 const questionsLi = document.querySelectorAll(".questions");
 const previousButton = document.getElementById("previous");
-let index = questionsLi.length / 2;
+let index = 0;
 const ul = document.getElementById("ulQues");
 const main = document.getElementById("js_main");
 const buttons = document.getElementById("js_buttons");
-
-let y = window.innerHeight / 2;
+console.log(ul.offsetHeight)
+let y = window.innerHeight / 3;
 let li = document.getElementById("js_li");
 let liHeight = li.clientHeight;
 console.log(liHeight);
 
 ul.style.top = `${y}px`;
 buttons.style.top = `${y + ul.clientHeight}px`;
-
-ul.scrollTop = (ul.scrollHeight - ul.clientHeight) / 2;
-
-// Initialize the initial opacity classes
-for (let i = questionsLi.length / 2; i < questionsLi.length; i++) {
-  if (i === index) {
-    questionsLi[i].className = "opacity-100";
-  } else {
-    questionsLi[i].className = "opacity-30";
-  }
-}
 
 nextButton.addEventListener("click", function (e) {
   e.preventDefault();
